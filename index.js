1dd818a13be0de3b9baa5b9e13c9e13fe2b236e4
Romifff <romain.magne@edhec.com>
2023-07-10 19:21:05 +0200

    Commit

--- index.js
+++ index.js
@@ -4,11 +4,18 @@
 // init project
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT || 3030;
+
+
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,7 +28,7 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 
@@ -30,3 +37,33 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get('/api/:date?', (req, res) => {
+  const regexp = /^(\d{13})?$/;
+  if (regexp.test(req.params.date)) {
+    const date = new Date(parseInt(req.params.date))
+    res.json({
+      "unix": date.getTime(),
+      "utc": date.toUTCString()
+    });
+  } else {
+    const date = new Date(req.params.date);
+    if (!isNaN(date.getDate())) {
+      res.json({
+        "unix": date.getTime(),
+        "utc": date.toUTCString()
+      });
+    }
+    else if(!req.params.date){
+      const defaultDate = new Date();
+      res.json({
+"unix": defaultDate.getTime(),
+"utc": defaultDate.toUTCString()
+      });
+    }
+    else {
+      res.json({ error: "Invalid Date" });
+    }
+  }
+})
+
